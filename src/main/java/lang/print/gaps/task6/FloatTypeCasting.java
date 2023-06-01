c047bb5d879797be439c8dacc923639028c214e2
mamula2023 <mamulashvili.2017@gmail.com>
2023-06-02 03:05:31 +0400

    pathetic

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int integerPart =(int) numberToBeRounded;
+        float floatingPart = numberToBeRounded-integerPart;
+
+        if(floatingPart>=0.5)
+            System.out.println(++integerPart);
+        else
+            System.out.println(integerPart);
     }
 }
